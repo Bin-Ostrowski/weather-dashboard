ca4a6371bbea244598e3ff845cd98daca705a7ea
Bin O <bin.ostrowski@gmail.com>
2022-06-12 16:34:31 -0700

    connected form input to weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,13 @@ var getCityLongLatt = function (cityName) {
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data.latt, data.longt);
-        // var cityLatt = data.latt;
-        // var cityLongt = data.longt;
+            getCurrentWeather(data, cityName);
         })
     })
   
 };
    
-//handle user input from form
+//handle user input from formEl
 var formSubmitHandler = function(event) {
     event.preventDefault();
     //get value from input element
@@ -27,33 +26,30 @@ var formSubmitHandler = function(event) {
         cityInputEl.value = "";
     } else {
         alert("Please enter a city.");
-    }
-}
+    };
+};
 
-// var displayCity = function (cityName, searchTerm) {
+//fetch for OpenWeather api for city / current weather conditions
+var getCurrentWeather = function (data) {
+    //format openWeather api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.latt + "&lon=" + data.longt + "&exclude=minutely,hourly,alerts&appid=4eb3f3ce5a058a8056f9cb0c3b28f9ea"
+
+    //make requst to the url 
+    fetch(apiUrl)
+        .then(function(response) {
+            response.json().then(function(data) {
+               console.log(data);
+                });
+            
+        })
+};
+
+
+//display city name
+// var displayCurrentWeather = function (cityName, searchTerm) {
 //     console.log(cityName);
 //     console.log(searchTerm);
 // }
-//activate search buton
-userFormEl.addEventListener("submit", formSubmitHandler);
-
-// //fetch for api
-// const getCurrentWeather = function () {
-//     //format openWeather api url
-//     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&appid=4eb3f3ce5a058a8056f9cb0c3b28f9ea"
-
-//     //make requst to the url
-//     fetch(apiUrl)
-//         .then(function(response) {
-//             response.json().then(function(data) {
-//                console.log(data, city);
-//                 });
-            
-//         })
-// }
-//response  city / current weather conditions
-
-//display city name
 //display current date
 //display an icon representation of weather conditions, 
     //the temperature, 
@@ -77,3 +73,5 @@ userFormEl.addEventListener("submit", formSubmitHandler);
 //retreive past city from local storage 
 //display as button under past searched cities.
 
+//activate search buton
+userFormEl.addEventListener("submit", formSubmitHandler);
